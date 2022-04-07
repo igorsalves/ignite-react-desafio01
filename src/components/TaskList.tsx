2d5f9be2e2c55bdf5a11a0ea01ed872e06851ee5
Igor Silva Alves <igorsaj@hotmail.com>
2022-04-06 21:29:00 -0300

    Finalizado Desafio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,38 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  function getRandomId(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const newTask: Task = {
+      id: getRandomId(1, 999999),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const editTask = tasks.map(task => task.id === id ? {
+      ...task, isComplete: !task.isComplete
+    } : task);
+
+    setTasks(editTask);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTask = tasks.filter(task => task.id !== id);
+    
+    setTasks(filteredTask);
   }
 
   return (
